79470e5e00d78745e429e5192ba9ba98538f7b7c
michel silva <michelalmeidasilva@outlook.pt>
2026-06-03 03:06:53 -0300

    feat(aws): ingest-lambda module + adopt existing function

---
+++ aws/modules/ingest-lambda/main.tf
@@ -0,0 +1,60 @@
+data "aws_iam_policy_document" "assume" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "this" {
+  name               = "${var.function_name}-role"
+  assume_role_policy = data.aws_iam_policy_document.assume.json
+}
+
+resource "aws_iam_role_policy_attachment" "basic" {
+  role       = aws_iam_role.this.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+data "aws_iam_policy_document" "ssm_read" {
+  statement {
+    actions   = ["ssm:GetParameter", "ssm:GetParameters"]
+    resources = var.ssm_parameter_arns
+  }
+  statement {
+    actions   = ["kms:Decrypt"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role_policy" "ssm_read" {
+  name   = "${var.function_name}-ssm-read"
+  role   = aws_iam_role.this.id
+  policy = data.aws_iam_policy_document.ssm_read.json
+}
+
+resource "aws_lambda_function" "this" {
+  function_name = var.function_name
+  package_type  = "Image"
+  image_uri     = var.image_uri
+  role          = aws_iam_role.this.arn
+  timeout       = 30
+  memory_size   = 512
+
+  environment {
+    variables = {
+      STORAGE_PROVIDER = "s3"
+      STORAGE_BUCKET   = var.storage_bucket_name
+      AWS_LWA_PORT     = "8080"
+      MONGODB_URI      = var.mongodb_uri
+      RABBITMQ_URL     = var.rabbitmq_url
+    }
+  }
+}
+
+resource "aws_lambda_function_url" "this" {
+  function_name      = aws_lambda_function.this.function_name
+  authorization_type = "NONE"
+}
